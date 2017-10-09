171d21cbe4f400f104604f363f9a234548372234
Alexander Hoppe <alexanderehoppe@gmail.com>
2017-10-09 18:01:07 -0400

    Finished BitSlice.t.v exhaustive testing

--- BitSlice.t.v
+++ BitSlice.t.v
@@ -12,11 +12,15 @@ module BitSliceTestHarness ();
 
     reg[3:0] inputs;
     reg[6:0] index;
+    reg testfailed;
 
     initial begin
     // Test bench code here.
     $dumpfile("BitSlice.vcd");
     $dumpvars(0,bit_slice);
+
+    // Initialize test failed to 0
+    testfailed = 0;
 
     // Test simpler gates, OR, NOR, NAND, AND, XOR
     // OR
@@ -25,6 +29,7 @@ module BitSliceTestHarness ();
         {CIN, A, B} = inputs; #1000 // Set inputs and wait
         // $display( "%b %b %b", index, A, B); //
         if (res != A|B) begin
+            testfailed = testfailed+1;
             $display("Test Case OR Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A|B);
         end
     end
@@ -35,6 +40,7 @@ module BitSliceTestHarness ();
         {CIN, A, B} = inputs; #1000 // Set inputs and wait
         // $display( "%b %b %b", index, A, B); //
         if (res != A~|B) begin
+            testfailed = testfailed+1;
             $display("Test Case NOR Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A~|B);
         end
     end
@@ -45,6 +51,7 @@ module BitSliceTestHarness ();
         {CIN, A, B} = inputs; #1000 // Set inputs and wait
         // $display( "%b %b %b", index, A, B); //
         if (res != A~&B) begin
+            testfailed = testfailed+1;
             $display("Test Case NAND Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A~&B);
         end
     end
@@ -55,6 +62,7 @@ module BitSliceTestHarness ();
         {CIN, A, B} = inputs; #1000 // Set inputs and wait
         // $display( "%b %b %b", index, A, B); //
         if (res != A&B) begin
+            testfailed = testfailed+1;
             $display("Test Case AND Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A&B);
         end
     end
@@ -65,8 +73,55 @@ module BitSliceTestHarness ();
         {CIN, A, B} = inputs; #1000 // Set inputs and wait
         // $display( "%b %b %b", index, A, B); //
         if (res != A^B) begin
+            testfailed = testfailed+1;
             $display("Test Case XOR Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A^B);
         end
+    end
+
+    // Test more complicated gates: ADD and SUB
+    // ADD
+    {ADD, SUB, XOR, AND, NAND, NOR, OR} = 7'b1000000; // Set SUB flag
+    for (inputs=4'b0; inputs<4'b1000; inputs=inputs+4'b1) begin
+        {CIN, A, B} = inputs; #1000 // Set inputs and wait
+        // $display( "%b %b %b", index, A, B); //
+        if (sum != A^B^CIN) begin
+            testfailed = testfailed+1;
+            $display("Test Case ADD Cin:%b A:%b B:%b Failed, Got Sum:%b Expected Sum:%b", CIN, A, B, sum, A^B^CIN);
+        end
+        if (sum != A^B^CIN) begin
+            testfailed = testfailed+1;
+            $display("Test Case ADD Cin:%b A:%b B:%b Failed, Got Res:%b Expected Res:%b", CIN, A, B, sum, A^B^CIN);
+        end
+        if (cout != (A&B)|((A^B)&CIN)) begin // (A&B)|((A^B)&CIN) is the correct carryout logic
+            testfailed = testfailed+1;
+            $display("Test Case ADD Cin:%b A:%b B:%b Failed, Got Cout:%b Expected Cout:%b", CIN, A, B, cout, (A&B)|((A^B)&CIN));
+        end
+    end
+
+    // SUB is identical to ADD but all of the B inputs to the test cases are inverted.
+    {ADD, SUB, XOR, AND, NAND, NOR, OR} = 7'b0100000; // Set SUB flag
+    for (inputs=4'b0; inputs<4'b1000; inputs=inputs+4'b1) begin
+        {CIN, A, B} = inputs; #1000 // Set inputs and wait
+        // $display( "%b %b %b", index, A, B); //
+        if (sum != A^(!B)^CIN) begin
+            testfailed = testfailed+1;
+            $display("Test Case SUB Cin:%b A:%b B:%b Failed, Got Sum:%b Expected Sum:%b", CIN, A, B, sum, A^(!B)^CIN);
+        end
+        if (sum != A^(!B)^CIN) begin
+            testfailed = testfailed+1;
+            $display("Test Case SUB Cin:%b A:%b B:%b Failed, Got Res:%b Expected Res:%b", CIN, A, B, sum, A^(!B)^CIN);
+        end
+        if (cout != (A&(!B))|((A^(!B))&CIN)) begin // (A&(!B))|((A^(!B))&CIN) is the correct carryout logic
+            testfailed = testfailed+1;
+            $display("Test Case SUB Cin:%b A:%b B:%b Failed, Got Cout:%b Expected Cout:%b", CIN, A, B, cout, (A&(!B))|((A^(!B))&CIN));
+        end
+    end
+
+    if(testfailed) begin
+        $display("%d Test Failures", testfailed);
+    end
+    else begin
+        $display("Tests Passed");
     end
 
     end
