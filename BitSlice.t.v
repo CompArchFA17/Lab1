90806040a764fb34960c258474f25bb3ec453d55
Alexander Hoppe <alexanderehoppe@gmail.com>
2017-10-09 17:29:20 -0400

    Added combinatorial gates testing in BitSlice.t.v and dummy code for bit slice

--- BitSlice.t.v
+++ BitSlice.t.v
@@ -11,18 +11,63 @@ module BitSliceTestHarness ();
     BitSlice bit_slice (cout, sum, res, ADD, SUB, XOR, AND, NAND, NOR, OR, A, B, CIN);
 
     reg[3:0] inputs;
+    reg[6:0] index;
 
     initial begin
     // Test bench code here.
     $dumpfile("BitSlice.vcd");
     $dumpvars(0,bit_slice);
 
-    // Test simpler gates first
-    for (inputs=4'b0; inputs<4'b100; inputs=inputs+4'b1) begin
-        {CIN, A, B} = inputs;
-        
+    // Test simpler gates, OR, NOR, NAND, AND, XOR
+    // OR
+    {ADD, SUB, XOR, AND, NAND, NOR, OR} = 7'b0000001; // Set OR flag
+    for (inputs=4'b0; inputs<4'b1000; inputs=inputs+4'b1) begin
+        {CIN, A, B} = inputs; #1000 // Set inputs and wait
+        // $display( "%b %b %b", index, A, B); //
+        if (res != A|B) begin
+            $display("Test Case OR Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A|B);
+        end
     end
 
+    // NOR
+    {ADD, SUB, XOR, AND, NAND, NOR, OR} = 7'b0000010; // Set OR flag
+    for (inputs=4'b0; inputs<4'b1000; inputs=inputs+4'b1) begin
+        {CIN, A, B} = inputs; #1000 // Set inputs and wait
+        // $display( "%b %b %b", index, A, B); //
+        if (res != A~|B) begin
+            $display("Test Case NOR Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A~|B);
+        end
+    end
+
+    // NAND
+    {ADD, SUB, XOR, AND, NAND, NOR, OR} = 7'b0000100; // Set OR flag
+    for (inputs=4'b0; inputs<4'b1000; inputs=inputs+4'b1) begin
+        {CIN, A, B} = inputs; #1000 // Set inputs and wait
+        // $display( "%b %b %b", index, A, B); //
+        if (res != A~&B) begin
+            $display("Test Case NAND Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A~&B);
+        end
+    end
+
+    // AND
+    {ADD, SUB, XOR, AND, NAND, NOR, OR} = 7'b0001000; // Set OR flag
+    for (inputs=4'b0; inputs<4'b1000; inputs=inputs+4'b1) begin
+        {CIN, A, B} = inputs; #1000 // Set inputs and wait
+        // $display( "%b %b %b", index, A, B); //
+        if (res != A&B) begin
+            $display("Test Case AND Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A&B);
+        end
+    end
+
+    // XOR
+    {ADD, SUB, XOR, AND, NAND, NOR, OR} = 7'b0010000; // Set OR flag
+    for (inputs=4'b0; inputs<4'b1000; inputs=inputs+4'b1) begin
+        {CIN, A, B} = inputs; #1000 // Set inputs and wait
+        // $display( "%b %b %b", index, A, B); //
+        if (res != A^B) begin
+            $display("Test Case XOR Cin:%b A:%b B:%b Failed, Got %b Expected %b", CIN, A, B, res, A^B);
+        end
+    end
 
     end
 
--- BitSlice.v
+++ BitSlice.v
@@ -1,9 +1,13 @@
 //Single bit slice module for ALU
 
-module BitSlice(
+module BitSlice
+(
     output cout, sum, res,
     input ADD, SUB, XOR, AND, NAND, NOR, OR, A, B, CIN
 );
-   
+    // TODO This is fake testing code
+        assign cout = 0;
+        assign res = 0;
+        assign sum = 0;
 
 endmodule
