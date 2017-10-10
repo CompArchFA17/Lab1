c2b0d82c73d48574a19e2949fff15f0e7c2d75ff
arianaolson419 <arianaolson419@gmail.com>
2017-10-10 15:10:46 -0400

    Add adder modules.

--- alu.v
+++ alu.v
@@ -1,5 +1,33 @@
 `define NOR nor #10
-`define OR not #20
+`define OR or #20
+`define AND and #20
+`define XOR xor #20
+
+
+// Implementation of a 1-bit full adder.
+module FullAdder1bit
+(
+    output sum, 
+    output carryout,
+    input a, 
+    input b0, 
+    input carryin,
+    input subtract
+);
+    wire cout1;
+    wire cout2;
+    wire sumAB;
+    wire b;
+
+    `XOR b0xorsubtract(b, b0, subtract);
+    `XOR AxorB(sumAB, a, b);
+    `XOR sumABxorCin(sum, sumAB, carryin);
+
+    `AND AandB(cout1, a, b);
+    `AND sumABandCin(cout2, sumAB, carryin);
+
+    `OR orcarries(carryout, cout1, cout2);
+endmodule
 
 module AddSub 
 (
@@ -8,11 +36,31 @@ output carryout,
 output zero,
 output overflow,
 input[31:0] operandA,
-input[31:0] operandB
+input[31:0] operandB,
+input subtract
 );
 
-	
+reg carryoutmid[30:0];
+reg zeromid[30:0];
+FullAdder1bit adderinit (result[0], carryoutmid[0], operandA[0], operandB[0], subtract, subtract);
+genvar i;
+for (i = 1; i < 31; i = i + 1) begin
+	FullAdder1bit addermid (result[i], carryoutmid[i], operandA[i], operandB[i], subtract, carryoutmid[i- 1]);
+end
+FullAdder1bit adderfinal (result[31], carryout, operandA[31], operandB[31], subtract, carryout);
+
+`XOR overflowdetection(overflow, carryoutmid[30], carryout);
+
+`NOR zeroinit(zeromid[0], result[0], result[1]);
+
+genvar j;
+for (j = 0; j < 29; j = j + 1) begin
+	`NOR (zeromid[j + 1],  zeromid[j], result[j + 1]);
+end
+
+`NOR (zero, zeromid[30], result[31]);
 endmodule
+
 
 
 module NOR
