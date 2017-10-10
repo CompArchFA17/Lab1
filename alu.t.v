4d71756a8817b71ac1052cc242e0dfc30ab40356
Jonah Spear <Jonahspear512@gmail.com>
2017-10-10 09:58:02 -0400

    made testing code a bit simpler

--- alu.t.v
+++ alu.t.v
@@ -23,16 +23,14 @@ module testALU ();
     if ((a ^ b) == out) begin
         passed_tests = passed_tests + 1;
         $display("Passed test with op: %b", op);
-        $display("a:   %b", a);
-        $display("b:   %b", b);
-        $display("out: %b", out);
     end
     else begin
         $display("Failed test with op: %b", op);
-        $display("a:   %b", a);
-        $display("b:   %b", b);
-        $display("out: %b", out);
     end
+    $display("a:   %b", a);
+    $display("b:   %b", b);
+    $display("out: %b", out);
+    
     // Test SLT
     op=3'b011;
     a=32'b00000000000000000000000000000001; b=32'b00000000000000000000000000000010;#1000
@@ -40,16 +38,13 @@ module testALU ();
     if ((a < b) == out) begin
         passed_tests = passed_tests + 1;
         $display("Passed test with op: %b", op);
-        $display("a:   %b", a);
-        $display("b:   %b", b);
-        $display("out: %b", out);
     end
     else begin
         $display("Failed test with op: %b", op);
-        $display("a:   %b", a);
-        $display("b:   %b", b);
-        $display("out: %b", out);
     end
+    $display("a:   %b", a);
+    $display("b:   %b", b);
+    $display("out: %b", out);
 
 
     $display("%2d/%2d Test Cases Passed", passed_tests, tests);
