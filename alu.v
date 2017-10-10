dd9201584e8acd56003a1510bfb1673f4ccd028b
arianaolson419 <arianaolson419@gmail.com>
2017-10-09 21:41:17 -0400

    add macros for delay.

--- alu.v
+++ alu.v
@@ -1,3 +1,6 @@
+`define NOR nor #10
+`define OR not #20
+
 module AddSub 
 (
 output[31:0] result,
@@ -24,7 +27,24 @@ input[31:0] operandB
 
 genvar i;
 for (i = 0; i < 32; i = i + 1) begin
-nor norgate (result[i], operandA[i], operandB[i]);
+	`NOR norgate (result[i], operandA[i], operandB[i]);
+end
+
+endmodule
+
+module OR
+(
+output[31:0] result,
+output carryout,
+output zero,
+output overflow,
+input[31:0] operandA,
+input[31:0] operandB
+);
+
+genvar i;
+for (i = 0; i < 32; i = i + 1) begin
+	`OR orgate(result[i], operandA[i], operandB[i]);
 end
 
 endmodule
