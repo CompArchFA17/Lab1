a6b0e86f57ec4d7adb9b6ece1731352d107da3b4
arianaolson419 <arianaolson419@gmail.com>
2017-10-09 21:38:40 -0400

    Start modules for add/subtract and or/nor

--- alu.v
+++ alu.v
@@ -1,3 +1,34 @@
+module AddSub 
+(
+output[31:0] result,
+output carryout,
+output zero,
+output overflow,
+input[31:0] operandA,
+input[31:0] operandB
+);
+
+	
+endmodule
+
+
+module NOR
+(
+output[31:0] result,
+output carryout,
+output zero,
+output overflow,
+input[31:0] operandA,
+input[31:0] operandB
+);
+
+genvar i;
+for (i = 0; i < 32; i = i + 1) begin
+nor norgate (result[i], operandA[i], operandB[i]);
+end
+
+endmodule
+
 module ALU
 (
 output[31:0]  result,
