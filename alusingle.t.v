4bf8853655aeb8616d63ee92df39c90203108c02
Bryan Werth <bryan.werth@students.olin.edu>
2017-10-10 21:00:34 -0400

    Fixed the Test Bench

--- alusingle.t.v
+++ alusingle.t.v
@@ -1,10 +1,10 @@
 // Single Bit ALU testbench
 `timescale 1 ns / 1 ps
-`include "alusinglebit.v"
+`include "alu_single_bit.v"
 
 module testSingleBitALU ();
-    reg operandA, operandB, carryin, command;
-    wire result, carryout, correct;
+    reg operandA, operandB, carryin, command, correct;
+    wire result, carryout;
 
     ALUsinglebit alusinglebittest(result,carryout,operandA,operandB,carryin,command); 
 
@@ -12,83 +12,83 @@ module testSingleBitALU ();
     $display("command | carryin operandA operandB| result carryout | correct");
     
     //Add
-    command = `b000; carryin = 0; operandA=0; operandB=0; correct = result==0 #50 
+    command = 3'b000; carryin = 0; operandA=0; operandB=0; correct = result==0; #50 
     $display("%b| %b %b  %b|%b  %b| %b", command, carryin, operandA, operandB, result, carryout, correct);
-    command = `b000; carryin = 0; operandA=0; operandB=1; correct = result==1 #50 
+    command = 3'b000; carryin = 0; operandA=0; operandB=1; correct = result==1; #50 
     $display("%b| %b %b  %b|%b  %b| %b", command, carryin, operandA, operandB, result, carryout, correct);   
-    command = `b000; carryin = 0; operandA=1; operandB=0; correct = result==1 #50 
+    command = 3'b000; carryin = 0; operandA=1; operandB=0; correct = result==1; #50 
     $display("%b| %b %b  %b|%b  %b| %b", command, carryin, operandA, operandB, result, carryout, correct);
-    command = `b000; carryin = 0; operandA=1; operandB=1; correct = result==0 #50 
+    command = 3'b000; carryin = 0; operandA=1; operandB=1; correct = result==0; #50 
     $display("%b| %b %b  %b|%b  %b| %b", command, carryin, operandA, operandB, result, carryout, correct);
    
-    command = `b000; carryin = 1; operandA=0; operandB=0; correct = result==1 #50 
+    command = 3'b000; carryin = 1; operandA=0; operandB=0; correct = result==1; #50 
     $display("%b| %b %b  %b|%b  %b| %b", command, carryin, operandA, operandB, result, carryout, correct);
-    command = `b000; carryin = 1; operandA=0; operandB=1; correct = result==0 #50 
+    command = 3'b000; carryin = 1; operandA=0; operandB=1; correct = result==0; #50 
     $display("%b| %b %b  %b|%b  %b| %b", command, carryin, operandA, operandB, result, carryout, correct);  
-    command = `b000; carryin = 1; operandA=1; operandB=0; correct = result==0 #50 
+    command = 3'b000; carryin = 1; operandA=1; operandB=0; correct = result==0; #50 
     $display("%b| %b %b  %b|%b  %b| %b", command, carryin, operandA, operandB, result, carryout, correct);
-    command = `b000; carryin = 1; operandA=1; operandB=1; correct = result==1 #50 
+    command = 3'b000; carryin = 1; operandA=1; operandB=1; correct = result==1; #50 
     $display("%b| %b %b  %b|%b  %b| %b", command, carryin, operandA, operandB, result, carryout, correct);
 
 
     //SL
-    command = `b010; operandA=0; operandB=0; correct = result==0 #50 
+    command = 3'b010; operandA=0; operandB=0; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b010; operandA=0; operandB=1; correct = result==1 #50 
+    command = 3'b010; operandA=0; operandB=1; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);    
-    command = `b010; operandA=1; operandB=0; correct = result==0 #50 
+    command = 3'b010; operandA=1; operandB=0; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b010; operandA=1; operandB=1;correct = result==0 #50 
+    command = 3'b010; operandA=1; operandB=1;correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
     
     //XOR
-    command = `b011; operandA=0; operandB=0; correct = result==0 #50 
+    command = 3'b011; operandA=0; operandB=0; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b011; operandA=0; operandB=1;correct = result==1 #50 
+    command = 3'b011; operandA=0; operandB=1;correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);    
-    command = `b011; operandA=1; operandB=0; correct = result==1 #50 
+    command = 3'b011; operandA=1; operandB=0; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b011; operandA=1; operandB=1; correct = result==0 #50 
+    command = 3'b011; operandA=1; operandB=1; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
 
     //AND
-    command = `b100; operandA=0; operandB=0; correct = result==0 #50 
+    command = 3'b100; operandA=0; operandB=0; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b100; operandA=0; operandB=1; correct = result==0 #50 
+    command = 3'b100; operandA=0; operandB=1; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);    
-    command = `b100; operandA=1; operandB=0; correct = result==0 #50 
+    command = 3'b100; operandA=1; operandB=0; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b100; operandA=1; operandB=1; correct = result==1 #50 
+    command = 3'b100; operandA=1; operandB=1; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
 
     //NAND
-    command = `b100; operandA=0; operandB=0; correct = result==1 #50 
+    command = 3'b100; operandA=0; operandB=0; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b100; operandA=0; operandB=1; correct = result==1 #50 
+    command = 3'b100; operandA=0; operandB=1; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);    
-    command = `b100; operandA=1; operandB=0; correct = result==1 #50 
+    command = 3'b100; operandA=1; operandB=0; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b100; operandA=1; operandB=1; correct = result==0 #50 
+    command = 3'b100; operandA=1; operandB=1; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
 
     //OR
-    command = `b100; operandA=0; operandB=0; correct = result===0 #50 
+    command = 3'b100; operandA=0; operandB=0; correct = result===0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b100; operandA=0; operandB=1; correct = result==1 #50 
+    command = 3'b100; operandA=0; operandB=1; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);    
-    command = `b000; operandA=1; operandB=0; correct = result==1 #50 
+    command = 3'b000; operandA=1; operandB=0; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b000; operandA=1; operandB=1; correct = result==1 #50 
+    command = 3'b000; operandA=1; operandB=1; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
     
     //NOR
-    command = `b000; operandA=0; operandB=0; correct = result==1 #50 
+    command = 3'b000; operandA=0; operandB=0; correct = result==1; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b000; operandA=0; operandB=1; correct = result==0 #50 
+    command = 3'b000; operandA=0; operandB=1; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);    
-    command = `b000; operandA=1; operandB=0; correct = result==0 #50 
+    command = 3'b000; operandA=1; operandB=0; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
-    command = `b000; operandA=1; operandB=1; correct = result==0 #50 
+    command = 3'b000; operandA=1; operandB=1; correct = result==0; #50 
     $display("%b| %b  %b|%b  %b| %b", command, operandA, operandB, result, carryout, correct);
   	
 
