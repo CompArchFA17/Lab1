75e26ad0ea609d47d9faa28cf4a4dcc9f61043ef
Kimber <kimberly.winter@students.olin.edu>
2017-10-10 22:06:10 -0400

    ALU32bit.v

--- ALU32bit.v
+++ ALU32bit.v
@@ -2,6 +2,11 @@
 
 `include "adder_subtracter.v"
 `include "slt.v" 
+`include "and_32bit.v"
+`include "nand_32bit.v"
+`include "xor_32bit.v"
+`include "nor_32bit.v"
+`include "or_32bit.v"
 
 module ALUcontrolLUT //Ben Hill's code
 (
@@ -21,7 +26,12 @@ module ALUcontrolLUT //Ben Hill's code
 );
 
 adder_subtracter addsub0(addsub[31:0],cout,flag,a[31:0],b[31:0],ALUcommand[2:0]);
+xor_32bit xor0(xorin[31:0],a[31:0],b[31:0]);
 full_slt_32bit slt0(slt[31:0],a[31:0],b[31:0]);
+and_32bit and0(andin[31:0],a[31:0],b[31:0]);
+nand_32bit nand0(nandin[31:0],a[31:0],b[31:0]);
+nor_32bit nor0(norin[31:0],a[31:0],b[31:0]);
+or_32bit or0(orin[31:0],a[31:0],b[31:0]);
 
 
   always @(ALUcommand) 
@@ -29,7 +39,7 @@ full_slt_32bit slt0(slt[31:0],a[31:0],b[31:0]);
     if (ALUcommand[0]==0 && ALUcommand[1]==0 && ALUcommand[2]==0)
     	finalsignal[31:0]=addsub[31:0]; 
     if (ALUcommand[0]==0 && ALUcommand[1]==0 && ALUcommand[2]==0)
-    	finalsignal[31:0]=xorin[31:0];
+    	finalsignal[31:0]=addsub[31:0];
     if(ALUcommand[0]==0 && ALUcommand[1]==1 && ALUcommand[2]==0)
     	finalsignal[31:0]=xorin[31:0];
     if(ALUcommand[0]==1 && ALUcommand[1]==1 && ALUcommand[2]==0)
