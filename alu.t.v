d87a104142e3ecca2b5e3d2bbefb177f326e35b8
Sung Park <sungwoo.park@students.olin.edu>
2017-10-10 22:26:52 -0400

    Add ADD and SUB test cases

--- alu.t.v
+++ alu.t.v
@@ -1,22 +1,53 @@
-module ALUcontrolLUT
-(
-output reg[2:0] 	muxindex,
-output reg	invertB,
-output reg	othercontrolsignal,
-...
-input[2:0]	ALUcommand
-)
+`timescale 1 ns / 1 ps
+`include "alu.v"
 
-  always @(ALUcommand) begin
-    case (ALUcommand)
-      `ADD:  begin muxindex = 0; invertB=0; othercontrolsignal = ?; end    
-      `SUB:  begin muxindex = 0; invertB=1; othercontrolsignal = ?; end
-      `XOR:  begin muxindex = 1; invertB=0; othercontrolsignal = ?; end    
-      `SLT:  begin muxindex = 2; invertB=?; othercontrolsignal = ?; end
-      `AND:  begin muxindex = 3; invertB=?; othercontrolsignal = ?; end    
-      `NAND: begin muxindex = 3; invertB=?; othercontrolsignal = ?; end
-      `NOR:  begin muxindex = ?; invertB=?; othercontrolsignal = ?; end    
-      `OR:   begin muxindex = ?; invertB=?; othercontrolsignal = ?; end
-    endcase
-  end
-endmodule
+`define ADD  3'd0
+`define SUB  3'd1
+`define XOR  3'd2
+`define SLT  3'd3
+`define AND  3'd4
+`define NAND 3'd5
+`define NOR  3'd6
+`define OR   3'd7
+
+module testALU():
+    reg [31:0] operandA;
+    reg [31:0] operandB;
+    reg [2:0] command;
+    wire [31:0] result;
+    wire carryout;
+    wire zero;
+    wire overflow;
+
+    ALU alu (result, carryout, zero, overflow, operandA, operandB, command);
+
+    initial begin
+    $display("testing ADD")
+
+    $display("operandA operandB command | result carryout zero overflow | expected outputs");
+    
+    a=32'b00000000000000000000000000000010;b=32'b00000000000000000000000000000001;c=ADD; #1000
+    $display("%b  %b  %b  |  %b  %b  %b  %b | 00000000000000000000000000000011  0  0  0", result, carryout, zero, overflow);
+
+    a=32'b11111111111111111111111111111111;b=32'b11111111111111111111111111111111;c=ADD; #1000
+    $display("%b  %b  %b  |  %b  %b  %b  %b | 11111111111111111111111111111110 1 0 0", result, carryout, zero, overflow);
+
+    a=32'b00000000000000000000000000000000;b=32'b00000000000000000000000000000000;c=ADD; #1000
+    $display("%b  %b  %b  |  %b  %b  %b  %b | 00000000000000000000000000000000 0 1 0", result, carryout, zero, overflow);
+
+    a=32'b01111111111111111111111111111111;b=32'b00000000000000000000000000000001;c=ADD; #1000
+    $display("%b  %b  %b  |  %b  %b  %b  %b | 10000000000000000000000000000000  0  0  1", result, carryout, zero, overflow);
+
+    $display("testing SUB")
+
+    a=32'b00000000000000000000000000000011;b=32'b00000000000000000000000000000001;c=SUB; #1000
+    $display("%b  %b  %b  |  %b  %b  %b  %b  |  00000000000000000000000000000010  0  0  0", result, carryout, zero, overflow);
+
+    a=32'b10000000000000000000000000000000;b=32'b00000000000000000000000000000001;c=SUB; #1000
+    $display("%b  %b  %b  |  %b  %b  %b  %b  |  01111111111111111111111111111111  1  0  1", result, carryout, zero, overflow);
+
+    a=32'b00000000000000000000000000000000;b=32'b00000000000000000000000000000000;c=SUB; #1000
+    $display("%b  %b  %b  |  %b  %b  %b  %b  |  00000000000000000000000000000000  0  1  0", result, carryout, zero, overflow);
+
+    a=32'hFFFFFFFF;b=32'hFFFFFFFF;c=SUB; #1000
+    $display("%b  %b  %b  |  %b  %b  %b  %b  |  32'h00000000  1  1  0", result, carryout, zero, overflow);
