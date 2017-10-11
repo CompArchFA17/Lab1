ef7802f24971d5d32fd886d70e97c4f2f653616b
arianaolson419 <arianaolson419@gmail.com>
2017-10-11 09:27:43 -0400

    Copy in Prava's modules.

--- alu.v
+++ alu.v
@@ -61,6 +61,94 @@ end
 `NOR (zero, zeromid[30], result[31]);
 endmodule
 
+module alu32bitxor
+(
+output[31:0]  result,
+output        carryout,
+output        zero,
+output        overflow,
+input[31:0]   operandA,
+input[31:0]   operandB
+);
+
+genvar i;
+generate
+  for (i = 0; i < 32; i = i + 1)
+  begin: ripple
+    `XOR xorgate (result[i], operandA[i], operandB[i]);
+  end
+endgenerate
+
+//doesn't need to set a flag
+assign carryout = 0;
+assign zero = 0;
+assign overflow = 0;
+
+endmodule
+
+//the set-less-than command, uses the 32-bit subtractor
+module alu32bitslt
+(
+output[31:0]  result,
+output        carryout,
+output        zero,
+output        overflow,
+input[31:0]   operandA,
+input[31:0]   operandB
+);
+wire[31:0] subresult;
+wire subcarryout;
+wire subzero;
+wire suboverflow;
+
+//set invertB to 1 because subtraction is needed
+AddSub subtractor (subresult, subcarryout, subzero, suboverflow, operandA, operandB, 1);
+
+assign result = 32'b0;
+
+`XOR final (result[0], subresult[0], suboverflow);
+
+//doesn't need to set a flag
+assign carryout = 0;
+assign zero = 0;
+assign overflow = 0;
+
+endmodule
+
+module alu32bitandn
+(
+output[31:0]  result,
+output        carryout,
+output        zero,
+output        overflow,
+input[31:0]   operandA,
+input[31:0]   operandB,
+input         othercontrolsignal
+);
+wire interresult[31:0];
+
+genvar i;
+generate
+  for (i = 0; i < 32; i = i + 1)
+  begin: ripple0
+    `AND andgate (interresult[i], operandA[i], operandB[i]);
+  end
+endgenerate
+
+genvar j;
+generate
+  for (j = 0; j < 32; j = j + 1)
+  begin: ripple1
+    `XOR final (result[j], othercontrolsignal, interresult[j]);
+  end
+endgenerate
+
+//doesn't need to set a flag
+assign carryout = 0;
+assign zero = 0;
+assign overflow = 0;
+
+endmodule
 
 module NOROR
 (
