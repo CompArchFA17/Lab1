24f38442149ed64110954db80ce54d5df66c3ac9
Your Name Goes Here <robert.siegel@students.olin.edu>
2017-10-11 19:53:02 -0400

    ALU test cases

--- adder_subtracter.t.v
+++ adder_subtracter.t.v
@@ -13,39 +13,48 @@ module test32bitAdder();
     adder_subtracter adder0(ans[31:0], carryout, overflow, a[31:0], b[31:0], carryin[2:0]);
 
     initial begin
-        a=32'b00000000000000000000000000000001;
-        b=32'b00000000000000000000000000000001;
-        carryin=3'b001; #5000 
-        $display("%b  %b  %b |  %b  %b %b | ", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+       $display("Input A                          Input B                          Command | Output                         Flag  Carryout");
 
+        // Addition tests
         a=32'b00000000000000000000000000000001;
         b=32'b00000000000000000000000000000001;
         carryin=3'b100; #5000 
-        $display("%b  %b  %b |  %b  %b %b | ", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
-
-        a=32'b00000000001000000000000000000000;
-        b=32'b00000000000000000000000010000000;
-        carryin=3'b001; #5000 
-        $display("%b  %b  %b |  %b  %b %b | ", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
-
-        a=32'b10000000001000010000000000000000;
-        b=32'b10000000000000010000000010000000;
-        carryin=3'b001; #5000 
-        $display("%b  %b  %b |  %b  %b %b | ", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
-
-        a=32'b10000000000000000000000000000001;
-        b=32'b10000000000000000000000000000001;
-        carryin=3'b001; #5000 
-        $display("%b  %b  %b |  %b  %b %b | ", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
-
-        a=32'b00000000000000000000000000000000;
-        b=32'b10000000000000000000000000000001;
-        carryin=3'b001; #5000 
-        $display("%b  %b  %b |  %b  %b %b | ", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+        $display("%b  %b  %b |  %b  %b    %b", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
 
         a=32'b10000000000000000000000000000001;
         b=32'b10000000000000000000000000000001;
         carryin=3'b000; #5000 
-        $display("%b  %b  %b |  %b  %b %b | ", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+        $display("%b  %b  %b |  %b  %b    %b", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+
+        // Subtraction Tests
+        a=32'b00000000000000000000000000000001;
+        b=32'b00000000000000000000000000000001;
+        carryin=3'b001; #5000 
+        $display("%b  %b  %b |  %b  %b    %b", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+
+        a=32'b00000000001000000000000000000000;
+        b=32'b00000000000000000000000010000000;
+        carryin=3'b001; #5000 
+        $display("%b  %b  %b |  %b  %b    %b", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+
+        a=32'b10000000001000010000000000000000;
+        b=32'b10000000000000010000000010000000;
+        carryin=3'b001; #5000 
+        $display("%b  %b  %b |  %b  %b    %b", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+
+        a=32'b10000000000000000000000000000001;
+        b=32'b10000000000000000000000000000001;
+        carryin=3'b001; #5000 
+        $display("%b  %b  %b |  %b  %b    %b", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+
+        a=32'b00000000000000000000000000000000;
+        b=32'b10000000000000000000000000000001;
+        carryin=3'b001; #5000 
+        $display("%b  %b  %b |  %b  %b    %b", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
+
+        a=32'b10000000000000000000000000000001;
+        b=32'b10000000000000000000000000000001;
+        carryin=3'b000; #5000 
+        $display("%b  %b  %b |  %b  %b    %b", a[31:0],b[31:0],carryin,ans[31:0],carryout, overflow);
     end
 endmodule
