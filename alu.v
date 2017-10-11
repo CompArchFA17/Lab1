529bccd2e3658fa748958570168e93c8aaba7f4f
Christopher Aring <ChrisssAring@gmail.com>
2017-10-11 18:12:10 -0400

    commit
    new alu

--- alu.v
+++ alu.v
@@ -1,9 +1,33 @@
+`include "adder.v"
+`include "multiplexer.v"
+`include "lut.v"
+
 `define AND and #30
 `define OR or #30
 `define NOT not #10
 `define XOR xor #50
 `define NAND nand #20
 `define NOR nor #20
+
+module oneBitALU
+(
+	input bNegate,
+	input a,
+	input b,
+	input [2:0] operation,
+	input carryin,
+	output result,
+	output carryout,
+
+);
+	wire b, bNeagate;
+	
+	`NOT (nB, b);
+
+
+    wire a, b, nB, aAndB, AOrB;
+
+endmodule
 
 module ALU
 (
@@ -15,5 +39,5 @@ input[31:0]   operandA,
 input[31:0]   operandB,
 input[2:0]    command
 );
-	// Your code here
+
 endmodule
