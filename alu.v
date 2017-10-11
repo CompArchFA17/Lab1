004861291ffeccb6509fb0b0a48ce8d7b6875f1f
changjun <ailuropoda0@gmail.com>
2017-10-11 03:58:26 -0400

    add test case

--- alu.v
+++ alu.v
@@ -1,12 +1,18 @@
+`define AND and #50
+`define OR  or #50
+`define NOT not #50
+
 module ALU
 (
-output[31:0]  result,
-output        carryout,
-output        zero,
-output        overflow,
-input[31:0]   operandA,
-input[31:0]   operandB,
-input[2:0]    command
+	output[31:0]  result,
+	output        carryout,
+	output        zero,
+	output        overflow,
+	input[31:0]   operandA,
+	input[31:0]   operandB,
+	input[2:0]    command
 );
-	// Your code here
+
+
+
 endmodule
