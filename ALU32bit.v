25ef21ba93adad1b88b3d6b9123005252c534d45
Kimber <kimberly.winter@students.olin.edu>
2017-10-10 21:56:27 -0400

    More ALU32 bit

--- ALU32bit.v
+++ ALU32bit.v
@@ -8,7 +8,7 @@ module ALUcontrolLUT //Ben Hill's code
 	output reg	[31:0]controlsignal, //final 32-bit output
 	output cout, //addsub only
 	output flag, //addsub only
-	output [31:0]finalsignal,
+	output reg[31:0] finalsignal,
 	input [2:0]ALUcommand,
 	input [31:0]a,
 	input [31:0]b,
@@ -19,34 +19,28 @@ module ALUcontrolLUT //Ben Hill's code
 	input [31:0]orin
 
 );
-adder_subtracter addsub0([31:0]addsub,cout,flag,[31:0]a,[31:0]b,[2:0]ALUcommand);
-full_slt_32bit slt0=([31:0]slt,[31:0]a,[31:0]b);
+
+adder_subtracter addsub0(addsub[31:0],cout,flag,a[31:0],b[31:0],ALUcommand[2:0]);
+full_slt_32bit slt0(slt[31:0],a[31:0],b[31:0]);
 
 
   always @(ALUcommand) 
   	begin
-    case (ALUcommand==[0,0,0])
-    	controlsignal==addsub;
-  	endcase
-  	case (ALUcommand==[0,0,1])
-    	controlsignal==addsub;
-  	endcase
-    case (ALUcommand==[0,1,0])
-    	controlsignal==xor;
-  	endcase
-    case (ALUcommand==[0,1,1])
-    	controlsignal==slt;
-  	endcase
-  	case (ALUcommand==[1,0,0])
-    	controlsignal==and;
-  	endcase
-  	case (ALUcommand==[1,0,1])
-    	controlsignal==nand;
-  	endcase
-  	case (ALUcommand==[1,1,0])
-    	controlsignal==nor;
-  	endcase
-  	case (ALUcommand==[1,0,0])
-    	controlsignal==or;
-  	endcase
+    if (ALUcommand[0]==0 && ALUcommand[1]==0 && ALUcommand[2]==0)
+    	finalsignal[31:0]=addsub[31:0]; 
+    if (ALUcommand[0]==0 && ALUcommand[1]==0 && ALUcommand[2]==0)
+    	finalsignal[31:0]=xorin[31:0];
+    if(ALUcommand[0]==0 && ALUcommand[1]==1 && ALUcommand[2]==0)
+    	finalsignal[31:0]=xorin[31:0];
+    if(ALUcommand[0]==1 && ALUcommand[1]==1 && ALUcommand[2]==0)
+    	finalsignal[31:0]=slt[31:0];
+  	if(ALUcommand[0]==0 && ALUcommand[1]==0 && ALUcommand[2]==1)
+    	finalsignal[31:0]=andin[31:0];
+  	if(ALUcommand[0]==1 && ALUcommand[1]==0 && ALUcommand[2]==1)
+    	finalsignal[31:0]=nandin[31:0];
+  	if(ALUcommand[0]==0 && ALUcommand[1]==1 && ALUcommand[2]==1)
+    	finalsignal[31:0]=norin[31:0];
+	else
+    	finalsignal[31:0]=orin[31:0];
+    end
 endmodule
