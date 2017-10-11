93991548bccde86e9f2382d296dc7aa592aa12d5
KaitlynKeil <kaitlyn.keil@students.olin.edu>
2017-10-11 18:12:28 -0400

    Adding comments to timing test

--- alu_timing_test.t.v
+++ alu_timing_test.t.v
@@ -1,6 +1,4 @@
 `include "alu.v"
-// TODO: investigate, uncommenting this makes everything break.
-// `timescale 1 ns / 1 ps
 
 `define ADD  3'd0
 `define SUB  3'd1
@@ -11,8 +9,9 @@
 `define NOR  3'd6
 `define OR   3'd7
 
-module testALU ();
-  // Your test code here
+module test_timing_ALU ();
+  // This runs a single test to see the timing for each operation. The output should be a file called "alu_timing.vcd" which can
+  //  be used with GTKWave to see propagation delays.
     reg[2:0] address;
     reg signed [31:0] a,b;
     wire carryout, overflow, zero;
@@ -24,7 +23,7 @@ module testALU ();
 
     $dumpfile("alu_timing.vcd");
     $dumpvars(0,alu);
-    
+
     address = `SUB; a = 32'd10; b = 32'd10; #5000
     $display("SUB %d %d ", a, b);
     if ((out !== 32'd0) || (carryout !== 1) || (overflow !== 0)) $display("*** SUB %d %d failed. out: %d carryout: %d overflow: %d", a, b, out, carryout, overflow);
