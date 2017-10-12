ba38a26787d6ae2397071e7d922ca40293a02255
Christopher Aring <ChrisssAring@gmail.com>
2017-10-11 20:09:05 -0400

    more ALU
    more ALU

--- alu.v
+++ alu.v
@@ -1,6 +1,7 @@
 `include "adder.v"
 `include "multiplexer.v"
 `include "lut.v"
+`include "aluFullBit.v"
 
 `define AND and #30
 `define OR or #30
@@ -9,25 +10,6 @@
 `define NAND nand #20
 `define NOR nor #20
 
-module oneBitALU
-(
-	input bNegate,
-	input a,
-	input b,
-	input [2:0] operation,
-	input carryin,
-	output result,
-	output carryout,
-
-);
-	wire b, bNeagate;
-	
-	`NOT (nB, b);
-
-
-    wire a, b, nB, aAndB, AOrB;
-
-endmodule
 
 module ALU
 (
@@ -39,5 +21,45 @@ input[31:0]   operandA,
 input[31:0]   operandB,
 input[2:0]    command
 );
+
+wire [31:0] operandA, operandB, result, cIn, cOut;
+wire [2:0] command;
+
+wire [1:0] select;
+
+ALULut (select, invert, command);
+
+//invert B
+
+aluFullBit (result[0], cOut[0], operandA[0], operandB[0], 1'b0, command[0], select[1:0]);
+
+
+genvar i;
+    generate
+        for (i=1; i < 32; i=i+1) begin : aluBits
+            aluFullBit (result[i], cOut[i], operandA[i], operandB[i], cIn[i], command[0], select[1:0]
+        end
+    endgenerate
+	
+wire partialOverflow;
+`XOR (partialOverflow, cOut[30], cOut[31]);
+
+wire [1:0] nSelect;
+`NOT (notSel[0], sel[0]);
+`NOT (notSel[1], sel[1]);
+
+and #40 (carryout, carryoutSlice[31], notSel[0], notSel[1]);
+and #40 (overflow, initialOverflow, notSel[0], notSel[1]);
+
+wire [30:0] ors;
+`OR (ors[0], result[0], result[1]);
+
+generate
+	for (i=0; i < 30; i=i+1) begin : zeroFlag
+		`OR (ors[i+1], result[i+1], ors[i]);
+	end
+endgenerate
+
+`NOT (zero, ors[30]);
 
 endmodule
