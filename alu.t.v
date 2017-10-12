85bccef5299c458fc08b6d04dcf4f2a6b3e20136
Alexander Hoppe <alexanderehoppe@gmail.com>
2017-10-11 23:45:20 -0400

    Stubbed out test harness

---
+++ alu.t.v
@@ -0,0 +1,30 @@
+//Test harness for testing 32 bit ALU
+
+`include "alu.v"
+
+module ALUTestHarness ();
+
+    s;lkhasdfhSDLFKJA
+    SDLKFJASD;FLKJ
+    ASDLFKJ
+    ASD'LFKJS
+    ASLDKFJASD;FLKJ
+    SA'DLKFJ
+    ;SDLJF
+    ASD;;KLFJA;SLDKFJASLD'KFJ
+    ASDFJ
+    ASD;LKFLJASD;FLJASD;LFJKA
+    SD;LJA
+    SD;LFJA
+    SD;LFJ
+    AS;DLFJ
+    A;SDLKLJFFASD;;KFKJAS
+    D;FLJADFHADFH
+    ADFJHQD
+    BRT
+    HTZDFH
+    AFTHADFGED
+    gDcvaer
+    g
+
+endmodule
