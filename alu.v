dbc9ca99b4321829a890e05fef252669d9c3b976
Prava Dhulipalla <prava@students.olin.edu>
2017-10-11 21:26:55 -0400

    ALU almost implemented

--- alu.v
+++ alu.v
@@ -3,7 +3,6 @@
 `define AND and #20
 `define XOR xor #20
 
-
 // Implementation of a 1-bit full adder.
 module FullAdder1bit
 (
@@ -44,21 +43,19 @@ wire carryoutmid[30:0];
 wire zeromid[30:0];
 FullAdder1bit adderinit (result[0], carryoutmid[0], operandA[0], operandB[0], subtract, subtract);
 genvar i;
-for (i = 1; i < 31; i = i + 1) begin
-	FullAdder1bit addermid (result[i], carryoutmid[i], operandA[i], operandB[i], subtract, carryoutmid[i- 1]);
-end
-FullAdder1bit adderfinal (result[31], carryout, operandA[31], operandB[31], subtract, carryout);
+generate
+  for (i = 1; i < 31; i = i + 1)
+  begin: ripple
+  	FullAdder1bit addermid (result[i], carryoutmid[i], operandA[i], operandB[i], subtract, carryoutmid[i- 1]);
+  end
+endgenerate
+FullAdder1bit adderfinal (result[31], carryout, operandA[31], operandB[31], subtract, carryoutmid[30]);
 
 `XOR overflowdetection(overflow, carryoutmid[30], carryout);
 
 `NOR zeroinit(zeromid[0], result[0], result[1]);
 
-genvar j;
-for (j = 0; j < 29; j = j + 1) begin
-	`NOR (zeromid[j + 1],  zeromid[j], result[j + 1]);
-end
-
-`NOR (zero, zeromid[30], result[31]);
+`NOR norall(zero, result[31:0], 32'b0);
 endmodule
 
 module alu32bitxor
@@ -80,9 +77,9 @@ generate
 endgenerate
 
 //doesn't need to set a flag
-assign carryout = 0;
-assign zero = 0;
-assign overflow = 0;
+// assign carryout = 0;
+// assign zero = 0;
+// assign overflow = 0;
 
 endmodule
 
@@ -104,9 +101,9 @@ wire suboverflow;
 //set invertB to 1 because subtraction is needed
 AddSub subtractor (subresult, subcarryout, subzero, suboverflow, operandA, operandB, 1);
 
-assign result = 32'b0;
+assign result[31:1] = 31'b0;
 
-`XOR final (result[0], subresult[0], suboverflow);
+`XOR final (result[0], subresult[31], suboverflow);
 
 //doesn't need to set a flag
 assign carryout = 0;
@@ -163,14 +160,20 @@ input invertnor
 
 wire norres[31:0];
 genvar i;
-for (i = 0; i < 32; i = i + 1) begin
+generate
+  for (i = 0; i < 32; i = i + 1)
+  begin: ripple0
 	`NOR norgate (norres[i], operandA[i], operandB[i]);
-end
+  end
+endgenerate
 
 genvar j;
-for (j = 0; j < 32; j = j + 1) begin
+generate
+  for (j = 0; j < 32; j = j + 1)
+  begin: ripple1
 	`XOR final (result[j], invertnor, norres[j]);
-end
+  end
+endgenerate
 
 buf setcarryout (carryout, 'b0);
 buf setzero (zero, 'b0);
@@ -178,14 +181,14 @@ buf setoverflow (overflow, 'b0);
 endmodule
 
 // defining command numbers
-`define ADD  3'd0
-`define SUB  3'd1
-`define XOR  3'd2
-`define SLT  3'd3
-`define AND  3'd4
-`define NAND 3'd5
-`define NOR  3'd6
-`define OR   3'd7
+`define CADD  3'd0
+`define CSUB  3'd1
+`define CXOR  3'd2
+`define CSLT  3'd3
+`define CAND  3'd4
+`define CNAND 3'd5
+`define CNOR  3'd6
+`define COR   3'd7
 
 // implementing a control logic LUT to determine ALU operation
 module ALUcontrolLUT
@@ -198,24 +201,24 @@ input[2:0]	ALUcommand
 
   always @(ALUcommand) begin
     case (ALUcommand)
-      `ADD:  begin muxindex = 0; invertB=0; othercontrolsignal = 0; end
-      `SUB:  begin muxindex = 0; invertB=1; othercontrolsignal = 0; end
-      `XOR:  begin muxindex = 1; invertB=0; othercontrolsignal = 0; end
-      `SLT:  begin muxindex = 2; invertB=0; othercontrolsignal = 0; end
-      `AND:  begin muxindex = 3; invertB=0; othercontrolsignal = 0; end
-      `NAND: begin muxindex = 3; invertB=0; othercontrolsignal = 1; end
-      `NOR:  begin muxindex = 4; invertB=0; othercontrolsignal = 1; end
-      `OR:   begin muxindex = 4; invertB=0; othercontrolsignal = 0; end
+      `CADD:  begin muxindex = 0; invertB=0; othercontrolsignal = 0; end
+      `CSUB:  begin muxindex = 0; invertB=1; othercontrolsignal = 0; end
+      `CXOR:  begin muxindex = 1; invertB=0; othercontrolsignal = 0; end
+      `CSLT:  begin muxindex = 2; invertB=0; othercontrolsignal = 0; end
+      `CAND:  begin muxindex = 3; invertB=0; othercontrolsignal = 0; end
+      `CNAND: begin muxindex = 3; invertB=0; othercontrolsignal = 1; end
+      `CNOR:  begin muxindex = 4; invertB=0; othercontrolsignal = 1; end
+      `COR:   begin muxindex = 4; invertB=0; othercontrolsignal = 0; end
     endcase
   end
 endmodule
 
 // defining macros for second LUT
-`define ADDSUB 3'd0
-`define XOR 3'd1
-`define SLT 3'd2
-`define ANDNAND 3'd3
-`define NOROR 3'd4
+`define MADDSUB 3'd0
+`define MXOR 3'd1
+`define MSLT 3'd2
+`define MANDNAND 3'd3
+`define MNOROR 3'd4
 
 // decides which operation to take based on the results of the previous look up table
 module ALUoutputLUT
@@ -225,7 +228,10 @@ input[31:0] operandB,
 input[2:0] muxindex,
 input invertB,
 input othercontrolsignal,
-output reg[31:0] result
+output reg[31:0] result,
+output reg carryout,
+output reg zero,
+output reg overflow
 );
 wire[31:0] resAddsub;
 wire[31:0] resXor;
@@ -233,22 +239,41 @@ wire[31:0] resSlt;
 wire[31:0] resAndnand;
 wire[31:0] resNoror;
 
+wire carryoutAddSub;
+wire carryoutXor;
+wire carryoutSLT;
+wire carryoutAND;
+wire carryoutOR;
 
-AddSub dut0 (resAddsub, carryout, zero, overflow, operandA, operandB, invertB);
-alu32bitxor dut1 (resXor, carryout, zero, overflow, operandA, operandB);
-alu32bitslt dut2 (resSlt, carryout, zero, overflow, operandA, operandB);
-alu32bitandn dut3 (resAndnand, carryout, zero, overflow, operandA, operandB, othercontrolsignal);
-NOROR dut4 (resNoror, carryout, zero, overflow, operandA, operandB, othercontrolsignal);
+wire zeroAddSub;
+wire zeroXor;
+wire zeroSLT;
+wire zeroAND;
+wire zeroOR;
 
-  always @(muxindex) begin
-    case(muxindex)
-      `ADDSUB: begin result = resAddsub; end
-      `XOR: begin result = resXor; end
-      `SLT: begin result = resSlt; end
-      `ANDNAND: begin result = resAndnand; end
-      `NOROR: begin result = resNoror; end
-    endcase
-  end
+wire overflowAddSub;
+wire overflowXor;
+wire overflowSLT;
+wire overflowAND;
+wire overflowOR;
+
+AddSub dut0 (resAddsub, carryoutAddSub, zeroAddSub, overflowAddSub, operandA, operandB, invertB);
+alu32bitxor dut1 (resXor, carryoutXor, zeroXor, overflowXor, operandA, operandB);
+alu32bitslt dut2 (resSlt, carryoutSLT, zeroSLT, overflowSLT, operandA, operandB);
+alu32bitandn dut3 (resAndnand, carryoutAND, zeroAND, overflowAND, operandA, operandB, othercontrolsignal);
+NOROR dut4 (resNoror, carryoutOR, zeroOR, overflowOR, operandA, operandB, othercontrolsignal);
+
+always @(muxindex or resAddsub or resXor or resSlt or resAndnand or resNoror) begin
+ case(muxindex)
+    `MADDSUB: begin  result = resAddsub;   carryout = carryoutAddSub;  zero = zeroAddSub;  overflow = overflowAddSub; end
+    `MXOR: begin     result = resXor;      carryout = carryoutXor;     zero = zeroXor;     overflow = overflowXor; end
+    `MSLT: begin     result = resSlt;      carryout = carryoutSLT;     zero = zeroSLT;     overflow = overflowSLT; end
+    `MANDNAND: begin result = resAndnand;  carryout = carryoutAND;     zero = zeroAND;     overflow = overflowAND; end
+    `MNOROR: begin   result = resNoror;    carryout = carryoutOR;      zero = zeroOR;      overflow = overflowOR; end
+  endcase
+end
+
+
 endmodule
 
 //module to run the alu
@@ -262,5 +287,37 @@ input[31:0]   operandA,
 input[31:0]   operandB,
 input[2:0]    command
 );
-	// Your code here
+
+wire[2:0]   muxindex;
+wire  invertB;
+wire othercontrolsignal;
+
+ALUcontrolLUT controlLookup (muxindex, invertB, othercontrolsignal, command);
+
+ALUoutputLUT outputLookup (operandA, operandB, muxindex, invertB, othercontrolsignal, result, carryout, zero, overflow);
+
+endmodule
+
+
+module TEST();
+  reg[31:0] operandA;
+  reg[31:0] operandB;
+  reg control;
+  reg[2:0] command;
+
+  wire[31:0] result;
+  wire carryout;
+  wire zero;
+  wire overflow;
+
+   ALU alu(result, carryout, zero, overflow, operandA, operandB, command);
+
+  initial begin
+    operandA = 32'b10101010101010101010101010101010; operandB = 32'b01000000000000000000000000001010; command = 3'b111; #100000
+
+    $displayb("operandA: %b", operandA);
+    $displayb("operandB: %b", operandB);
+    $displayb("result:   %b", result);
+
+  end
 endmodule
