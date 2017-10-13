01e44d4a2603a90d54d9733c1c5803503d771d61
Tobias Shapinsky <tobiasshapinsky@gmail.com>
2017-10-13 00:50:21 -0400

    added two more test cases

--- alu.t.v
+++ alu.t.v
@@ -22,11 +22,21 @@ module testALU();
         if (result != 0 || overflow || !zero) begin
             $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
         end
+        a = 31'h7fffffff;
+        b = 1; #1000000
+        if (result != 31'h80000000 || !overflow || zero) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
         
         command = 3'd1;
         a = 4;
         b = 4; #1000000
         if (result != 0 || overflow || !zero) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = 31'h80000000;
+        b = 1; #1000000
+        if (result != 31'h7fffffff || !overflow || zero) begin
             $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
         end
     end
