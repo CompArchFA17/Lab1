e130349c5740de7032326fc4e1e48c59e116c4fe
Sung Park <sungwoo.park@students.olin.edu>
2017-10-12 21:35:37 -0400

    edit 3 bit mux test files

--- 3bitMux.t.v
+++ 3bitMux.t.v
@@ -1,9 +1,9 @@
-`define NANDgate nand #320
-`define ANDgate and #330
-`define NORgate nor #320
-`define ORgate  or  #330
+`define NANDgate nand #10
+`define ANDgate and #20
+`define NORgate nor #10
+`define ORgate  or  #20
 `define NOTgate not #10
-`define XORgate xor #650
+`define XORgate xor #30
 
 `timescale 1 ns / 1 ps
 `include "3bitMux.v"
@@ -17,15 +17,15 @@ module test3BitMux ();
 
     initial begin
     $display("addr inputs | Output");
-    addr=3`b000;inputs=8'b00010000; #1000
+    addr=3'b000;inputs=8'b00010000; #1000
     $display("%b    %b | %b", addr, inputs, out);
-    addr=3`b000;inputs=8'b10000001; #1000
+    addr=3'b000;inputs=8'b10000001; #1000
     $display("%b    %b | %b", addr, inputs, out);
-    addr=3`b000;inputs=8'b00000000; #1000
+    addr=3'b000;inputs=8'b00000000; #1000
     $display("%b    %b | %b", addr, inputs, out);
-    addr=3`b011;inputs=8'b01000001; #1000
+    addr=3'b011;inputs=8'b01000001; #1000
     $display("%b    %b | %b", addr, inputs, out);
-    addr=3`b010;inputs=8'b00001101; #1000
+    addr=3'b010;inputs=8'b00001101; #1000
     $display("%b    %b | %b", addr, inputs, out);
     end
 endmodule
