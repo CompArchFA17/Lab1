e43e86c78cff90fe3903b8b703fa0ba47d8b9ac8
Tobias Shapinsky <tobiasshapinsky@gmail.com>
2017-10-13 01:10:49 -0400

    added tests for all operations

--- alu.t.v
+++ alu.t.v
@@ -16,6 +16,7 @@ module testALU();
     initial begin
       $dumpfile("alu.vcd");
       $dumpvars(0, testALU);
+        //Addition tests
         command = 3'd0;
         a = -4;
         b = 4; #1000000
@@ -28,6 +29,7 @@ module testALU();
             $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
         end
         
+        //Subtraction tests
         command = 3'd1;
         a = 4;
         b = 4; #1000000
@@ -37,6 +39,94 @@ module testALU();
         a = 31'h80000000;
         b = 1; #1000000
         if (result != 31'h7fffffff || !overflow || zero) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        
+        //XOR tests
+        command = 3'd2;
+        a = 4'b1001;
+        b = 4'b1010; #1000000
+        if (result != a^b) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = 4'b0000;
+        b = 4'b1111; #1000000
+        if (result != a^b) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        
+        //SLT tests
+        command = 3'd3;
+        a = -8;
+        b = -16; #1000000
+        if (!result) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = 8;
+        b = 16; #1000000
+        if (result) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = 8;
+        b = -16; #1000000
+        if (!result) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = -8;
+        b = 16; #1000000
+        if (result) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        
+        //AND tests
+        command = 3'd4;
+        a = 4'b1001;
+        b = 4'b1010; #1000000
+        if (result != a&b) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = 4'b0000;
+        b = 4'b1111; #1000000
+        if (result != a&b) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        
+        //AND tests
+        command = 3'd5;
+        a = 4'b1001;
+        b = 4'b1010; #1000000
+        if (result != ~(a&b)) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = 4'b0000;
+        b = 4'b1111; #1000000
+        if (result != ~(a&b)) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        
+        //NOR tests
+        command = 3'd6;
+        a = 4'b1001;
+        b = 4'b1010; #1000000
+        if (result != ~(a|b)) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = 4'b0000;
+        b = 4'b1111; #1000000
+        if (result != ~(a|b)) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        
+        //OR tests
+        command = 3'd7;
+        a = 4'b1001;
+        b = 4'b1010; #1000000
+        if (result != (a|b)) begin
+            $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
+        end
+        a = 4'b0000;
+        b = 4'b1111; #1000000
+        if (result != (a|b)) begin
             $display("FAIL %d %d %d |cmd: %d |ovf: %d |0: %d |co: %d", a, b, result, command, overflow, zero, carryout);
         end
     end
